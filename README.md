c312d7693882c90bb0e8653f0d4790d415506bb9
wangliyang <wangliyang@dobest.com>
2025-10-15 19:06:21 +0800

    feat(spider): 重构手机爬虫并增强参数提取功能
    - 重命名爬虫类名 MobileZil为 MobileZolSpider
    - 更新 User-Agent 和请求头以适配目标网站
    -优化 XPath 提取逻辑，增加异常处理和默认值设置- 添加多层级参数页面跳转支持（参数页→完整参数页）
    - 增强关键手机参数的提取兼容性（上市日期、系统版本、CPU/GPU型号等）
    - 改进数据库连接管理，加入心跳查询保持连接活跃
    - 扩展数据表结构，新增 CPU 型号、GPU 型号、操作系统等字段
    - 调整下载延迟配置，降低请求间隔提高抓取效率
    - 修复数据插入时的 SQL 语法错误和字段缺失问题
    - 增加日志记录便于调试和监控爬虫运行状态

--- README.md
+++ README.md
@@ -10,9 +10,9 @@
 
         SET FOREIGN_KEY_CHECKS = 0;
 
-        DROP TABLE IF EXISTS  `spider_moble_zol`;
+        DROP TABLE IF EXISTS  `spider_moblie_zol`;
 
-        CREATE TABLE `spider_moble_zol` (
+        CREATE TABLE `spider_moblie_zol` (
           `id` int(11) NOT NULL AUTO_INCREMENT,
           `phone_name` varchar(32) DEFAULT '' COMMENT '手机名称',
           `phone_price` float DEFAULT NULL COMMENT '参考价格',
@@ -20,11 +20,14 @@
           `phone_parameter_url` varchar(512) DEFAULT '' COMMENT '参数详情链接',
           `phone_x` int(11) DEFAULT '0' COMMENT '分辨率宽',
           `phone_y` int(11) DEFAULT '0' COMMENT '分辨率高',
-          `phone_system` varchar(32) DEFAULT '' COMMENT '系统\n',
+          `os` varchar(32) DEFAULT '' COMMENT '操作系统',
+          `vendor_os` varchar(32) DEFAULT '' COMMENT '厂商操作系统',
           `phone_size` varchar(32) DEFAULT '' COMMENT '主屏尺寸',
           `phone_info` varchar(512) DEFAULT '' COMMENT '手机基本信息',
           `phone_brand` varchar(32) DEFAULT '' COMMENT '手机品牌',
           `create_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+          `cpu_model` varchar(64) DEFAULT '' COMMENT 'CPU型号',
+          `gpu_model` varchar(64) DEFAULT '' COMMENT 'GPU型号',
           PRIMARY KEY (`id`)
         ) ENGINE=InnoDB AUTO_INCREMENT=901 DEFAULT CHARSET=utf8;
 
--- scrapy_mobile_zol/pipelines.py
+++ scrapy_mobile_zol/pipelines.py
@@ -5,67 +5,97 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymysql
+from twisted.internet import task
 
 
 class ScrapyMobileZolPipeline(object):
     def __init__(self):
         # 建立连接
         self.conn = pymysql.connect(
-            host='127.0.0.1',
-            port=3306,
-            user='lvjj',
-            password='123456',
-            db='mysql_lvjj',
+            host='10.225.137.189',
+            port=13306,
+            user='root',
+            password='Dobest2022!',
+            db='cloud_test_dev_0814',
             charset='utf8'
         )
 
         # 创建游标
         self.cursor = self.conn.cursor()
+        self.looping_call = task.LoopingCall(self.heartbeat_query)
+        self.looping_call.start(1.0)  # 每1秒执行一次
+
+    def heartbeat_query(self):
+        """心跳查询，保持连接活跃"""
+        try:
+            self.conn.ping(reconnect=True)
+            # 执行简单的查询保持连接
+            self.cursor.execute("SELECT 1")
+            result = self.cursor.fetchone()
+            print(f"Heartbeat query result: {result}")
+        except Exception as e:
+            print(f"Heartbeat query error: {e}")
 
     # def data_insert(self, phone_name, phone_price, phone_info_url, phone_parameter_url, phone_x, phone_y, phone_size, phone_info, phone_brand):
     def data_insert(self, **kwargs):
-        insert_sql = """
-                insert into spider_moble_zol(
-                phone_name,
-                phone_price,
-                phone_info_url,
-                phone_parameter_url,
-                phone_x,
-                phone_y,
-                phone_size,
-                phone_info,
-                phone_brand) 
-                VALUES("{}","{}","{}","{}",{},{},{},"{}","{}")
-                """.format(kwargs['phone_name'],
-                           kwargs['phone_price'],
-                           kwargs['phone_info_url'],
-                           kwargs['phone_parameter_url'],
-                           kwargs['phone_x'],
-                           kwargs['phone_y'],
-                           kwargs['phone_size'],
-                           kwargs['phone_info'],
-                           kwargs['phone_brand'])
-        # 执行插入数据到数据库操作
-        # print(insert_sql)
-        self.cursor.execute(insert_sql)
-        # 提交，不进行提交无法保存到数据库
-        self.conn.commit()
+        try:
+            # 在执行任何操作前，检查并重连
+            self.conn.ping(reconnect=True)
+            insert_sql = """
+                    insert into spider_mobile_zol(
+                    phone_name,
+                    phone_price,
+                    phone_info_url,
+                    phone_parameter_url,
+                    phone_x,
+                    phone_y,
+                    phone_size,
+                    phone_info,
+                    phone_brand,
+                    os,
+                    vendor_os,
+                    release_date,
+                    cpu_model,
+                    gpu_model) 
+                    VALUES("{}","{}","{}","{}",{},{},{},"{}","{}","{}","{}","{}","{}","{}")
+                    """.format(kwargs['phone_name'],
+                               kwargs['phone_price'],
+                               kwargs['phone_info_url'],
+                               kwargs['phone_parameter_url'],
+                               kwargs['phone_x'],
+                               kwargs['phone_y'],
+                               kwargs['phone_size'],
+                               kwargs['phone_info'],
+                               kwargs['phone_brand'],
+                               kwargs['os'],
+                               kwargs['vendor_os'],
+                               kwargs['release_date'],
+                               kwargs['cpu_model'],
+                               kwargs['gpu_model'])
+            # 执行插入数据到数据库操作
+            # print(insert_sql)
+            self.cursor.execute(insert_sql)
+            # 提交，不进行提交无法保存到数据库
+            self.conn.commit()
+        except Exception as e:
+            print(f"数据库插入错误: {e}")
+            self.conn.rollback()
 
     def data_select(self, phone_info_url):
-        select_sql = "SELECT * FROM spider_moble_zol WHERE phone_info_url = '{}' and phone_price > 0".format(phone_info_url)
+        select_sql = "SELECT * FROM spider_mobile_zol WHERE phone_info_url = '{}' and phone_price > 0".format(phone_info_url)
         self.cursor.execute(select_sql)
         res = self.cursor.fetchone()
         # print("res = ", res)
         return res
 
     def data_update_price(self, phone_info_url, phone_price):
-        select_sql = "SELECT * FROM spider_moble_zol WHERE phone_info_url = '{}' and phone_price = 0".format(
+        select_sql = "SELECT * FROM spider_mobile_zol WHERE phone_info_url = '{}' and phone_price = 0".format(
             phone_info_url)
         self.cursor.execute(select_sql)
         res = self.cursor.fetchone()
         # print("res = ", res)
         if res:
-            uplate_sql = "UPDATE spider_moble_zol SET phone_price={} WHERE phone_info_url = '{}' and phone_price = 0".format(phone_price, phone_info_url)
+            uplate_sql = "UPDATE spider_mobile_zol SET phone_price={} WHERE phone_info_url = '{}' and phone_price = 0".format(phone_price, phone_info_url)
 
             # print(uplate_sql)
             self.cursor.execute(uplate_sql)
@@ -88,7 +118,9 @@ class ScrapyMobileZolPipeline(object):
                 self.data_insert(phone_name=item['phone_name'], phone_price=item['phone_price'],
                                  phone_info_url=item['phone_info_url'], phone_parameter_url=item['phone_parameter_url'],
                                  phone_x=item['phone_x'], phone_y=item['phone_y'], phone_size=item['phone_size'],
-                                 phone_info=item['phone_info'], phone_brand=item['phone_brand'])
+                                 phone_info=item['phone_info'], phone_brand=item['phone_brand'],
+                                 os=item['os'], vendor_os=item['vendor_os'], release_date=item['release_date'],
+                                 cpu_model=item['cpu_model'], gpu_model=item['gpu_model'])
                 # self.data_insert(dict(item))
         # self.cursor.execute(insert_sql)
         # # 提交，不进行提交无法保存到数据库
